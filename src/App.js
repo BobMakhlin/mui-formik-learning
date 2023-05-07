b083917020ec9f3b7835cab9228d105b823129aa
Bob Makhlin <borismakhlin5@gmail.com>
2023-05-07 21:33:19 +0300

    Form

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ import * as Yup from "yup";
 import Textfield from "./components/Forms/TextField";
 import Select from "./components/Forms/Select";
 import DatePicker from "./components/Forms/DatePicker";
+import Checkbox from "./components/Forms/Checkbox";
+import SubmitButton from "./components/Forms/Button";
 import countries from "./data/countries.json";
+
+const TERMS_OF_SERVICE_ERROR_MESSAGE =
+  "The terms and conditions must be accepted";
 
 const INITIAL_FORM_STATE = {
   firstName: "",
@@ -19,6 +24,8 @@ const INITIAL_FORM_STATE = {
   country: "UA",
   arrivalDate: "",
   depatureDate: "",
+  message: "",
+  termsOfService: false,
 };
 
 const FORM_VALIDATION = Yup.object().shape({
@@ -35,9 +42,16 @@ const FORM_VALIDATION = Yup.object().shape({
   state: Yup.string().required("Required"),
   country: Yup.string().required("Required"),
   arrivalDate: Yup.date()
+    .typeError("Please enter a valid date")
     .min(new Date(2023, 5, 5))
     .required("Required"),
-  depatureDate: Yup.date().required("Required"),
+  depatureDate: Yup.date()
+    .typeError("Please enter a valid date")
+    .required("Required"),
+  message: Yup.string(),
+  termsOfService: Yup.boolean()
+    .isTrue(TERMS_OF_SERVICE_ERROR_MESSAGE)
+    .required(TERMS_OF_SERVICE_ERROR_MESSAGE),
 });
 
 function App() {
@@ -46,10 +60,11 @@ function App() {
       <Formik
         initialValues={{ ...INITIAL_FORM_STATE }}
         validationSchema={FORM_VALIDATION}
-        onSubmit={(values) => {
+        onSubmit={(values, {setSubmitting}) => {
+          console.log("sending a request to API...");
           console.log("values:", values);
+          setSubmitting(false);
         }}
-        validateOnBlur
       >
         <Form>
           <Grid container spacing={2}>
@@ -96,7 +111,30 @@ function App() {
               <Typography>Booking information</Typography>
             </Grid>
             <Grid item xs={6}>
-              <DatePicker name="arrivalDate" />
+              <DatePicker name="arrivalDate" label="Arrival Date" />
+            </Grid>
+            <Grid item xs={6}>
+              <DatePicker name="depatureDate" label="Depature Date" />
+            </Grid>
+            <Grid item xs={12}>
+              <Textfield
+                name="message"
+                label="Message"
+                multiline
+                rows={4}
+              ></Textfield>
+            </Grid>
+
+            <Grid item xs={12}>
+              <Checkbox
+                name="termsOfService"
+                legend="Terms Of Service"
+                label="I agree"
+              ></Checkbox>
+            </Grid>
+
+            <Grid item xs={12}>
+              <SubmitButton>Submit</SubmitButton>
             </Grid>
           </Grid>
         </Form>
