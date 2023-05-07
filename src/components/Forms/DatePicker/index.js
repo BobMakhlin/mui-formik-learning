8d312d2966febdfbd494ed156cc300605baa68f0
Bob Makhlin <borismakhlin5@gmail.com>
2023-05-07 20:17:11 +0300

    Refactoring

--- src/components/Forms/DatePicker/index.js
+++ src/components/Forms/DatePicker/index.js
@@ -2,18 +2,26 @@ import { DatePicker } from "@mui/x-date-pickers";
 import { useField } from "formik";
 import { useCallback } from "react";
 
+const SHOULD_VALIDATE = true;
+
 const DatePickerWrapper = ({ name, label }) => {
   const [field, meta, { setValue, setTouched }] = useField(name);
 
   const handleChange = useCallback(
     (date) => {
-      setValue(date, true);
+      setValue(date, SHOULD_VALIDATE);
     },
     [setValue]
   );
-  const markAsTouched = useCallback(() => {
+  const handleDatePickerClose = useCallback(() => {
     if (!meta.touched) {
-      setTimeout(() => setTouched(true, true));
+      // setTimeout to avoid validating an old value.
+      setTimeout(() => setTouched(true, SHOULD_VALIDATE));
+    }
+  }, [meta.touched, setTouched]);
+  const handleTextFieldBlur = useCallback(() => {
+    if (!meta.touched) {
+      setTouched(true, SHOULD_VALIDATE);
     }
   }, [meta.touched, setTouched]);
 
@@ -24,13 +32,13 @@ const DatePickerWrapper = ({ name, label }) => {
     variant: "outlined",
     ...field,
     onChange: handleChange,
-    onClose: markAsTouched,
+    onClose: handleDatePickerClose,
   };
   const textFieldConfig = {
     fullWidth: true,
     error: hasError,
     helperText: hasError ? meta.error : null,
-    onBlur: markAsTouched,
+    onBlur: handleTextFieldBlur,
   };
 
   return (
